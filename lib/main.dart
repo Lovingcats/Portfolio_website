b8913b75271a77887a776e3d6d359e76f1916552
조용제 <whehdrms6810@naver.com>
2023-02-06 00:10:17 +0900

    [ADD] sidebar

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,171 @@
-import 'package:flutter/material.dart';
+import 'dart:math' as math show pi;
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:collapsible_sidebar/collapsible_sidebar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+        designSize: const Size(400, 900),
+        builder: (BuildContext context, Widget? child) => const MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: '안녕하세요! 신입 모바일 개발자 조용제입니다',
+              home: Scaffold(
+                body: SidebarPage(),
+              ),
+            ));
+  }
+}
+
+class SidebarPage extends StatefulWidget {
+  const SidebarPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SidebarPageState createState() => _SidebarPageState();
+}
+
+class _SidebarPageState extends State<SidebarPage> {
+  late List<CollapsibleItem> _items;
+  late String _headline;
+  // ignore: prefer_final_fields
+  AssetImage _avatarImg = const AssetImage('images/profile.png');
+
+  @override
+  void initState() {
+    super.initState();
+    _items = _generateItems;
+    _headline = _items.firstWhere((item) => item.isSelected).text;
+  }
+
+  List<CollapsibleItem> get _generateItems {
+    return [
+      CollapsibleItem(
+        text: 'Dashboard',
+        icon: Icons.assessment,
+        onPressed: () => setState(() => _headline = 'DashBoard'),
+        isSelected: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      CollapsibleItem(
+        text: 'Ice-Cream',
+        icon: Icons.icecream,
+        onPressed: () => setState(() => _headline = 'Errors'),
+      ),
+      CollapsibleItem(
+        text: 'Search',
+        icon: Icons.search,
+        onPressed: () => setState(() => _headline = 'Search'),
+      ),
+      CollapsibleItem(
+        text: 'Notifications',
+        icon: Icons.notifications,
+        onPressed: () => setState(() => _headline = 'Notifications'),
+      ),
+      CollapsibleItem(
+        text: 'Settings',
+        icon: Icons.settings,
+        onPressed: () => setState(() => _headline = 'Settings'),
+      ),
+      CollapsibleItem(
+        text: 'Home',
+        icon: Icons.home,
+        onPressed: () => setState(() => _headline = 'Home'),
+      ),
+      CollapsibleItem(
+        text: 'Alarm',
+        icon: Icons.access_alarm,
+        onPressed: () => setState(() => _headline = 'Alarm'),
+      ),
+      CollapsibleItem(
+        text: 'Eco',
+        icon: Icons.eco,
+        onPressed: () => setState(() => _headline = 'Eco'),
+      ),
+      CollapsibleItem(
+        text: 'Event',
+        icon: Icons.event,
+        onPressed: () => setState(() => _headline = 'Event'),
+      ),
+      CollapsibleItem(
+        text: 'Email',
+        icon: Icons.email,
+        onPressed: () => setState(() => _headline = 'Email'),
+      ),
+      CollapsibleItem(
+        text: 'Face',
+        icon: Icons.face,
+        onPressed: () => setState(() => _headline = 'Face'),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: CollapsibleSidebar(
+        isCollapsed: MediaQuery.of(context).size.width <= 800,
+        items: _items,
+        avatarImg: _avatarImg,
+        title: 'Cho Yong Je',
+        onTitleTap: () {
+          WebView(
+            initialUrl: '',
+          );
+        },
+        body: _body(size, context),
+        backgroundColor: Colors.black,
+        selectedTextColor: Colors.limeAccent,
+        textStyle: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+        titleStyle: TextStyle(
+            fontSize: 20,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold),
+        toggleTitleStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        sidebarBoxShadow: [
+          BoxShadow(
+            color: Colors.indigo,
+            blurRadius: 20,
+            spreadRadius: 0.01,
+            offset: Offset(3, 3),
+          ),
+          BoxShadow(
+            color: Colors.green,
+            blurRadius: 50,
+            spreadRadius: 0.01,
+            offset: Offset(3, 3),
+          ),
+        ],
+      ),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  Widget _body(Size size, BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.blueGrey[50],
+      child: Center(
+        child: Transform.rotate(
+          angle: math.pi / 2,
+          child: Transform.translate(
+            offset: Offset(-size.height * 0.3, -size.width * 0.23),
+            child: Text(
+              _headline,
+              style: Theme.of(context).textTheme.headline1,
+              overflow: TextOverflow.visible,
+              softWrap: false,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
